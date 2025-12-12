7850867017b114b2d801158ffcd7c8863b6158eb
fangdudu <1042096700@qq.com>
2025-12-12 16:46:30 +0800

    feat(module): add python module breakpoint

--- backend/robot-service/src/main/java/com/iflytek/rpa/robot/entity/dto/SaveModuleDto.java
+++ backend/robot-service/src/main/java/com/iflytek/rpa/robot/entity/dto/SaveModuleDto.java
@@ -3,8 +3,15 @@ package com.iflytek.rpa.robot.entity.dto;
 import com.iflytek.rpa.base.entity.dto.OpenModuleDto;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class SaveModuleDto extends OpenModuleDto {
     String moduleContent; // 代码模块内容
     String robotId; // robotID
+
+    /**
+     * python模块断点记录
+     */
+    String breakpoint;
 }
